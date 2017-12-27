3b6a44c8285594124fdc85754dbf6d0637b0e3dc
Mark Said Camilleri <markscamilleri@gmail.com>
2017-12-27 15:05:20 +0100

    Changed title

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'CPS3232 Assignment' });
 });
 
 module.exports = router;
