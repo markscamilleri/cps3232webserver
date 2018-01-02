eea67e9da98fafe8e21621c921ae5e05d358411a
Mark Said Camilleri <markscamilleri@gmail.com>
2018-01-02 18:43:00 +0100

    key is a pem

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.use(function(err, req, res, next) {
 });
 
 var secureServer = https.createServer({
-    key: fs.readFileSync('./ssl/webserver.key'),
+    key: fs.readFileSync('./ssl/webserver.key.pem'),
     cert: fs.readFileSync('./ssl/webserver.cert.pem'),
     ca: fs.readFileSync('./ssl/ca-chain.cert.pem'),
     requestCert: true,
