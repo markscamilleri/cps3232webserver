c18497be6ff8bd33147ec5bea3614bf21062247c
Mark Said Camilleri <markscamilleri@gmail.com>
2018-01-03 22:51:47 +0100

    Changed "email" to "username"

--- config/passport.js
+++ config/passport.js
@@ -54,7 +54,7 @@ module.exports = function (passport) {
 
                     // check to see if theres already a user with that email
                     if (user) {
-                        return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
+                        return done(null, false, req.flash('signupMessage', 'That username is already taken.'));
                     } else {
 
                         // if there is no user with that email
@@ -83,16 +83,16 @@ module.exports = function (passport) {
     // by default, if there was no name, it would just be called 'local'
     passport.use('local-login', new LocalStrategy({
             // by default, local strategy uses username and password, we will override with email
-            usernameField: 'email',
+            usernameField: 'username',
             passwordField: 'password',
             passReqToCallback: true // allows us to pass back the entire request to the callback
         },
 
-        function (req, email, password, done) { // callback with email and password from our form
+        function (req, isername, password, done) { // callback with email and password from our form
 
-            // find a user whose email is the same as the forms email
+            // find a user whose isername is the same as the forms email
             // we are checking to see if the user trying to login already exists
-            User.findOne({'local.email': email}, function (err, user) {
+            User.findOne({'local.username': isername}, function (err, user) {
                 // if there are any errors, return the error before anything else
                 if (err)
                     return done(err);
