eb25dc3a855550d2d9ec2eabdb50e19b69f51308
Mark Said Camilleri <markscamilleri@gmail.com>
2018-01-15 01:53:13 +0100

    Added photos and printing

--- app/print.js
+++ app/print.js
@@ -3,18 +3,10 @@ var blobStream  = require('blob-stream');
 
 var printFileToPdf = function(file, res) {
     var doc = new PDF();
-    var stream = doc.pipe(res);
+    doc.pipe(res);
 
     doc.image(file, 0, 15, {width: 300});
     doc.end();
-
-    var toReturn;
-    stream.onfinish(function(){
-        var blob = stream.toBlob('application/pdf');
-        var url = stream.toBlobURL('application/pdf');
-    });
-
-    return url
 };
 
-module.exports = printFileToPdf;
+module.exports = {printFileToPdf: printFileToPdf};
--- app/routes.js
+++ app/routes.js
@@ -1,48 +1,74 @@
-module.exports = function(app, passport, fs, upload) {
+module.exports = function (app, passport, fs) {
+    const https = require('https');
+    const printer = require('./print');
+
     // Home Page
-    app.get('/', function(req, res) {
-	    console.log("Am Here");
+    app.get('/', function (req, res) {
+        console.log("Am Here");
         res.render(('index'));
     });
 
     // Login Page
-    app.get('/login', function(req, res) {
-       res.render(('login'), {message: req.flash('loginMessage')});
+    app.get('/login', function (req, res) {
+        res.render(('login'), {message: req.flash('loginMessage')});
     });
 
     // Process the login form
     app.post('/login', passport.authenticate('oauth2'));
 
     // Photos page
-    app.get('/photos',   passport.authenticate('oauth2', { failureRedirect: '/login' }), function(req, res) {
-        res.render('photos', {
-            rows: arrayOfN(fs.readdirSync('/images/' + req.user), 4),
-            message: req.flash('uploadMessage'),
-            user: req.user // get the user out of session and pass to template
-        });
+    app.get('/photos', passport.authenticate('oauth2', {failureRedirect: '/login'}), function (req, res) {
+        https.request({
+            host: '192.168.1.10',
+            port: '8443',
+            path: '/api/photos',
+            method: 'GET'
+        }, function (res) {
+            var files = '';
+
+            res.on('data', function (chunk) {
+                files += chunk;
+            });
+
+            res.on('end', function () {
+                res.render('photos', {
+                    files: files,
+                    message: req.flash('uploadMessage'),
+                    user: req.user // get the user out of session and pass to template
+                });
+            }).on('error', function (err) {
+                console.log('Error:' + err.message);
+            });
+        }).end();
     });
 
-    app.post('/photos', isLoggedIn, upload.single('file'), function (req, res) {
-        if (!fs.existsSync('./images/' + req.user)){
-            fs.mkdirSync('./images/' + req.user);
-        }
+    app.get('/print', passport.authenticate('oauth2', {failureRedirect: '/login'}), function (req, res) {
+        if (req.query.photo) {
+            https.request({
+                host: '192.168.1.10',
+                port: '8443',
+                path: '/api/photo/' + req.query.photo,
+                method: 'GET'
+            }, function (res) {
+                var data = '';
 
-        var tempPath = req.files.file.path,
-            targetPath = path.resolve('./images/' + req.user + '/' + req.files.file.name);
-        if (arrayIncludes(['.png', '.jpg', '.gif'], path.extname(req.files.file.name).toLowerCase())) {
-            fs.rename(tempPath, targetPath, function(err) {
-                if (err) throw err;
-                res.flash('uploadMessage', 'Upload successful');
-            });
+                res.on('data', function (chunk) {
+                    data += chunk;
+                });
+
+                res.on('end', function () {
+                    printer.printFileToPdf(data, res);
+                }).on('error', function (err) {
+                    console.log('Error:' + err.message);
+                });
+            }).end();
         } else {
-            res.flash('uploadMessage', 'Upload was not png, jpg or gif');
+            res.redirect('/');
         }
-
-        res.redirect('/photos')
     });
 
     // Logout
-    app.get('/logout', function(req, res) {
+    app.get('/logout', function (req, res) {
         req.logout();
         res.redirect('/');
     });
@@ -64,7 +90,7 @@ function arrayOfN(elements, n) {
     var toReturn = [];
     elements.reverse();
 
-    while(elements.length > 0) {
+    while (elements.length > 0) {
         var row = [];
         for (var j = 0; j < n; j++) {
             if (elements.length === 0)
@@ -80,8 +106,8 @@ function arrayOfN(elements, n) {
 }
 
 function arrayIncludes(array, toCheck) {
-    for(var elem in array){
-        if(elem === toCheck)
+    for (var elem in array) {
+        if (elem === toCheck)
             return true;
     }
 
